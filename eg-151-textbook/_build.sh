7ba7fc1b1f496a87216fe0bac56b6736f60162af
Chris Jobling <cpjobling@cpjobling.net>
2024-08-26 16:30:14 +0200

    Finished conversion of lectures_intro.qmd

--- eg-151-textbook/_build.sh
+++ eg-151-textbook/_build.sh
@@ -3,7 +3,7 @@
 function render_book_slides() {
   quarto render .
   mv _quarto.yml _quarto_book.yml
-  mv _qarto_revealjs.yml _quarto.yml
+  mv _quarto_revealjs.yml _quarto.yml
   quarto render .
   mv _quarto.yml _quarto_revealjs.yml
   mv _quarto_book.yml _quarto.yml
