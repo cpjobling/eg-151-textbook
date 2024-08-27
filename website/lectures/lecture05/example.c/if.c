bede033d570994e43dadd0ac4d62af02097fafdc
Chris Jobling <cpjobling@cpjobling.net>
2024-08-27 15:38:38 +0200

    Completed Lecture 01

---
+++ website/lectures/lecture05/example.c/if.c
@@ -0,0 +1,6 @@
+Statement_1;
+if (expression != 0) // in C anything that is not 0 is true
+{
+    Statement_2;
+}
+Statement_3;
--- website/lectures/week04/example.c/if.c
+++
@@ -1,6 +0,0 @@
-Statement_1;
-if (expression != 0) // in C anything that is not 0 is true
-{
-    Statement_2;
-}
-Statement_3;
