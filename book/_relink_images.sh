5257998650d2af8df04fb6d3151cf2db9c949a53
Chris Jobling <cpjobling@cpjobling.net>
2024-08-27 18:28:39 +0200

    updated structure again

---
+++ book/_relink_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Relink images
+echo "Relinking images for book"
+rm pictures/*.{png,jpg}
+ln -f ../website/lectures/pictures/* pictures
+ln -f ../website/lectures/lecture01/pictures/* pictures 
+ln -f ../website/lectures/lecture02/pictures/* pictures
+#ln -f ../website/lectures/lecture03/pictures/* pictures
