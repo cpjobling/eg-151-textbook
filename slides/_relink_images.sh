6d33f2504ce40945764ef0663d38cabbdd706599
Chris Jobling <cpjobling@cpjobling.net>
2024-08-29 10:30:55 +0200

    Completed slides for lecture 1

--- slides/_relink_images.sh
+++ slides/_relink_images.sh
@@ -4,6 +4,6 @@
 echo "Relinking images from lecture files"
 rm pictures/*.{png,jpg}
 ln -f ../website/lectures/pictures/* pictures
-ln -f ../website/lectures/lecture01/pictures/* pictures 
-ln -f ../website/lectures/lecture02/pictures/* pictures
-#ln -f ../website/lectures/lecture03/pictures/* pictures
+ln -f ../website/lectures/week01/pictures/* pictures 
+ln -f ../website/lectures/data_representation/pictures/* pictures
+#ln -f ../website/lectures/week02/pictures/* pictures
