45e22953f23ed74da763836e2c2402f33bdd00e2
Chris Jobling <cpjobling@cpjobling.net>
2024-08-29 14:55:49 +0200

    checkpoint: WIP on week02 slides and notes

--- book/_relink_images.sh
+++ book/_relink_images.sh
@@ -6,4 +6,4 @@ rm pictures/*.{png,jpg}
 ln -f ../website/lectures/pictures/* pictures
 ln -f ../website/lectures/week01/pictures/* pictures 
 ln -f ../website/lectures/data_representation/pictures/* pictures
-#ln -f ../website/lectures/week02/pictures/* pictures
+ln -f ../website/lectures/week02/pictures/* pictures
