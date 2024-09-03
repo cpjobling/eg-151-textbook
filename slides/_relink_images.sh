44cc2135652288eeab077b4ee2062cbb5b770a19
Chris Jobling <cpjobling@cpjobling.net>
2024-09-03 19:48:56 +0200

    WIP Lecture 03

--- slides/_relink_images.sh
+++ slides/_relink_images.sh
@@ -7,3 +7,4 @@ ln -f ../website/lectures/pictures/* pictures
 ln -f ../website/lectures/week01/pictures/* pictures 
 ln -f ../website/lectures/data_representation/pictures/* pictures
 ln -f ../website/lectures/week02/pictures/* pictures
+ln -f ../website/lectures/week03/pictures/* pictures
