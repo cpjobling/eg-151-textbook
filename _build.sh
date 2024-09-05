dad4bfc7b62112b7de658bf28df95fd3ece18a8f
Chris Jobling <cpjobling@cpjobling.net>
2024-09-05 10:10:18 +0200

    Baseline for Lecture 5

--- _build.sh
+++ _build.sh
@@ -11,6 +11,7 @@ rsync -avzd slides/_slides/ website/_build/html/slides
 
 
 echo "Building PDF"
+rm -rf book/_book/*
 quarto render book --to pdf
 cp book/_book/EG-151-Microcontrollers-2024-2025.pdf website/_build/html
 
