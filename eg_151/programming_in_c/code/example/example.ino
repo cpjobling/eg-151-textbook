5a2e53ad91c7781f1879ee4125648c965bc65beb
Chris Jobling <cpjobling@cpjobling.net>
2024-10-23 10:12:14 +0100

    Readied Lab 2 content

--- eg_151/programming_in_c/code/example/example.ino
+++ eg_151/programming_in_c/code/example/example.ino
@@ -4,13 +4,12 @@ byte counter1, counter2;
 // the setup function runs once when you press reset or power the board
 void setup() {
     // define counters
-    counter1 = counter2 = 0; // initialize counters
+    counter1 = 0; counter2 = 0; // initialize counters
 
     // initialize Ports B and C pins 0-5 as outputs.
-    DDRB = 0b00111111;
-    DDRC = 0b00111111;
+    DDRB = 0b00111111; DDRC = 0b00111111;
 
-    PORTB = PORTC = 0; // both ports start from zero
+    PORTB = 0; PORTC = 0; // both ports start from zero
 }
 
 // the loop function runs repeatedly forever
@@ -18,14 +17,14 @@ void loop() {
     PORTB = counter2; // counter2 is our "seconds" counter
     PORTC = counter1; // counter1 is our "minutes" counter
 
+    counter2++; // using the tip we learnt earlier
     if (counter2 > 59) {
         counter2 = 0;
+        counter1++;
         if (counter1 > 59) {
             counter1 = 0;
         }
-        counter1++;
     }
-    counter2++; // using the tip we learnt earlier
 
     delay(1000); // again, reduce this delay if necessary to speed things up.
 }
